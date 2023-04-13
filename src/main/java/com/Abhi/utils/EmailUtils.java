eefaa4a07f9fcdadaf26908a4d7997b622eabe53
naveen <naveensiliveru143@gmail.com>
2023-04-13 11:51:47 +0530

    Mini project added

---
+++ src/main/java/com/Abhi/utils/EmailUtils.java
@@ -0,0 +1,46 @@
+package com.Abhi.utils;
+
+import java.io.File;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailUtils {
+	
+	@Autowired
+	private JavaMailSender emailSender;
+	
+	public void sendEmailWithAttachment(String to,
+			String subject, 
+			String text,
+			File f) throws MessagingException {
+	    MimeMessage message = emailSender.createMimeMessage();
+
+	    // use the true flag to indicate you need a multipart message
+	    MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+	    helper.setFrom("naveensiliveru143@gmail.com");
+	    helper.setTo(to);
+	    helper.setSubject(subject);
+	    helper.setText(text);
+
+	    // attach the file
+	 //   FileSystemResource file = new FileSystemResource(new File(f));
+	    
+	    helper.addAttachment("plans", f);
+
+	    emailSender.send(message);
+	    System.out.println("Mail with attachment sent successfully");
+
+
+}
+
+
+}
